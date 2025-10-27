5b0b09000eb1ecc7be3b8536157d6caa05560594
Primolo <primo.locatelli@gmail.com>
2025-10-27 12:26:28 +0100

    Fix supabaseUrl formatting in supabaseClient.js

--- g70-share-app/src/supabaseClient.js
+++ g70-share-app/src/supabaseClient.js
@@ -1,7 +1,7 @@
 // ----------------------------------------------------
 // ⚠️ VOS CLÉS D'API SUPABASE (UNIQUE PAR PROJET)
 // ----------------------------------------------------
-const supabaseUrl = 'https://ikileeetvexzkybwzuxv.supabase.co '
+const supabaseUrl = 'https://ikileeetvexzkybwzuxv.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlraWxlZWV0dmV4emt5Ynd6dXh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2MjE3ODYsImV4cCI6MjA3NjE5Nzc4Nn0._Zabpl-p9hMGyQUVZIeNAz40qmpqTt4QM2yQo7YV0Fg'  
 
 // Le client est exporté pour être utilisé partout.
